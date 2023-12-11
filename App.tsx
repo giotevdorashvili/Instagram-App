64b3ac42783ac96ef2de555727b90297f078f779
giotevdorashvili <giorgi.tevdorashvili@makingscience.com>
2023-12-11 17:09:15 +0400

    Add: initial navigation setup

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
-import {Text} from 'react-native';
+import Router from './src/navigators/Router';
 
 function App(): JSX.Element {
-  return <Text>Hello</Text>;
+  return <Router />;
 }
 
 export default App;
---
+++ src/navigators/StackNavigator.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  NativeStackScreenProps,
+  createNativeStackNavigator,
+} from '@react-navigation/native-stack';
+
+import LogIn from '../screens/logIn/LogIn';
+import SignUp from '../screens/signUp/SignUp';
+
+export type ScreenProps<T extends keyof RootStackParamList> =
+  NativeStackScreenProps<RootStackParamList, T>;
+
+export type RootStackParamList = {
+  LogIn: undefined;
+  SignUp: {};
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="LogIn" component={LogIn} />
+      <Stack.Screen name="SignUp" component={SignUp} />
+    </Stack.Navigator>
+  );
+};
+
+export default StackNavigator;
